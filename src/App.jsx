25b93a48aec1d9f80f76ef35d63aae91ebc25238
Darsh D. Coding <darshdcoding@gmail.com>
2025-10-04 14:23:08 +0530

    Adding routes to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
+import {Fragment} from 'react'
 import {Route, Routes} from 'react-router'
-import './App.css'
 import { HomePage } from './Pages/HomePage'
-import { AnotherPage } from './Pages/AnotherPage'
+import { DestinationPage } from './Pages/DestinationPage.jsx'
+import {CrewPage} from "./Pages/CrewPage.jsx";
+import {TechnologyPage} from "./Pages/TechnologyPage.jsx";
+import './App.css'
 
 
 function App() {
 
   return (
-    <>
+    <Fragment>
       <Routes>
-        <Route
-            index
-            element={<HomePage />}
-        />
-          <Route
-              path="another"
-              element={<AnotherPage />}
-          />
+          <Route index element={<HomePage />} />
+          <Route path="destination" element={<DestinationPage />} />
+          <Route path="crew" element={<CrewPage />} />
+          <Route path="technology" element={<TechnologyPage />} />
       </Routes>
-    </>
+    </Fragment>
   )
 }
 
