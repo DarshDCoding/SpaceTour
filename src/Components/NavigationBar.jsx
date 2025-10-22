86fcf42fc839ed51a89bca96f2f92350d7f2d25d
Darsh D. Coding <darshdcoding@gmail.com>
2025-10-22 22:02:45 +0530

    adding working toggle hamburger button

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -1,13 +1,11 @@
-import {NavLink} from "react-router";
 import {Navlink} from "./Navlink.jsx";
 import {useState} from "react";
 
 export function NavigationBar() {
-    const [hamClicked,setHamClicked] = useState(false);
+    const [hamClicked, setHamClicked] = useState(true);
     const logoUrl = '/assets/shared/logo.svg';
-    const clickedFunc =()=>{
+    const clickedFunc = () => {
         setHamClicked(!hamClicked);
-        console.log(hamClicked);
     }
 
     return (
@@ -17,11 +15,12 @@ export function NavigationBar() {
                 <div className="relative left-nav-bar flex items-center gap-[3rem]">
                     <img src={logoUrl} alt="Logo" className=''/>
                     <div className="hidden md:hidden lg:hidden xl:block absolute left-25 z-20 ">
-                    <div className="w-[45vw] h-[1px] border-1 border-gray-500" />
+                        <div className="w-[45vw] h-[1px] border-1 border-gray-500"/>
                     </div>
                 </div>
 
-                <div className="right-nav-bar hidden md:flex md:w-[90%] lg:w-[70%] xl:w-[50%] h-full px-10  justify-end items-center gap-[2rem] bg-gray-500/15 backdrop-blur-md ">
+                <div
+                    className="right-nav-bar hidden md:flex md:w-[90%] lg:w-[70%] xl:w-[50%] h-full px-10  justify-end items-center gap-[2rem] bg-gray-500/15 backdrop-blur-md ">
                     <Navlink to={"/"} title={"00 HOME"}/>
                     <Navlink to={"/destination"} title={"01 DESTINATION"}/>
                     <Navlink to={"/crew"} title={"02 CREW"}/>
@@ -29,7 +28,11 @@ export function NavigationBar() {
                 </div>
                 <div className="flex md:hidden lg:hidden xl:hidden items-center px-8 ">
                     <button onClick={clickedFunc}>
-                    <img src={"/assets/shared/icon-hamburger.svg"} alt="hamburger_menu" className='h-8'/>
+                        {hamClicked ? (
+                            <img src={"/assets/shared/icon-hamburger.svg"} alt="hamburger_menu" className='h-6'/>
+                        ) : (
+                            <img src={"/assets/shared/icon-close.svg"} alt="hamburger_menu" className='h-6'/>
+                        )}
                     </button>
                 </div>
             </nav>
